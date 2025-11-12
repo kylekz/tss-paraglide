93637d5856a2f9f5152e7a3f90ae280512fd94e5
kylekz <kairu@team-reflex.com>
2025-11-13 06:14:47 +1300

    strip down to just server function

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,43 @@
-import { songsQuery } from '@/data/demo.punk-songs'
-import { useSuspenseQuery } from '@tanstack/react-query'
+import { getPunkSongs } from '@/data/demo.punk-songs'
 import { createFileRoute } from '@tanstack/react-router'
-import { Suspense } from 'react'
 
 export const Route = createFileRoute('/')({
   component: App,
-  loader: async ({ context }) => {
-    await context.queryClient.ensureQueryData(songsQuery)
+  loader: async () => {
+    const songs = await getPunkSongs()
+    console.log({ songs })
+    return { songs }
   },
 })
 
 function App() {
+  const { songs } = Route.useLoaderData()
+
   return (
     <div className="flex flex-col">
       <h2>Index</h2>
-      <Suspense fallback={<div>Loading...</div>}>
-        <PunkSongs />
-      </Suspense>
+      <div>
+        <h1>Punk Songs</h1>
+        <ul>
+          {songs.map((song) => (
+            <li key={song.id}>{song.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
 
-function PunkSongs() {
-  const data = useSuspenseQuery(songsQuery)
-  return (
-    <div>
-      <h1>Punk Songs</h1>
-      <ul>
-        {data.data.map((song) => (
-          <li key={song.id}>{song.name}</li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+// function PunkSongs() {
+//   const data = useSuspenseQuery(songsQuery)
+//   return (
+//     <div>
+//       <h1>Punk Songs</h1>
+//       <ul>
+//         {data.data.map((song) => (
+//           <li key={song.id}>{song.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
